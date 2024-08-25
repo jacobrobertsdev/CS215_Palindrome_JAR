02baea0af7eccc3aa56ac5e2dc9b6e4c28520604
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-08-25 14:54:30 -0500

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		PalindromeCheck p = new PalindromeCheck();
+		
+		p.isPalindrome("RaceCar");
+		p.isPalindrome("hello");
+		p.isPalindrome("r.a.c.e.c.a.r");
+		p.isPalindrome("sa---ippu353aki45345vi5ka5u5p5pias");
+		p.isPalindrome("a man a plan a canal panama");
+	}
+
+}
---
+++ src/PalindromeCheck.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+/**
+ * The PalindromeCheck class takes in a String and determines whether or not it is a palindrome.
+ * Ignores case, special characters, spaces, and digits.
+ * 
+ */
+public class PalindromeCheck {
+	
+	private Stack<Character> charStack;
+	
+	
+	/**
+	 * Default constructor. Creates an empty Stack of Characters
+	 * 
+	 */
+	public PalindromeCheck() {
+		charStack = new Stack<>();
+	}
+	
+	
+	/**
+	 * Tests if the input string is a palindrome.
+	 * 
+	 * @param input String to test for palindrome
+	 * @return true if the input String is a palindrome, false otherwise.
+	 */
+	public boolean isPalindrome(String input) {
+		
+		charStack.clear();
+		
+		char[] charArray = input.toCharArray();
+		
+		for(char c : charArray) {
+			if (Character.isLetter(c)) {
+				charStack.push(Character.toLowerCase(c));
+			}
+			
+		}
+		
+		for(char c : charArray) {
+			if(Character.isLetter(c) && charStack.pop() != Character.toLowerCase(c)){
+				System.out.println(input + " is not a palindrome!\n");
+				return false;
+			}
+		}
+		
+		System.out.println(input + " is a palindrome!\n");
+		return true;
+	}
+
+}
